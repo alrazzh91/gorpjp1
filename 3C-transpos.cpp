297d853eac0f6a7d2cb23cd7584decbf60b776d6
zeezaah <117928757+alrazzh91@users.noreply.github.com>
2022-12-31 06:14:37 +0700

    Create 3C-transpos.cpp

---
+++ 3C-transpos.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int N, M, i, j;
+    1<=N,M<= 10;
+    0<=i,j<=9;
+    int Array[10][10];
+    
+    cin>>N>>M;
+    
+    for (i=0; i<N; i++) {
+        for (j=0; j<M; j++) {
+            cin>>Array[i][j];
+        }
+    cout<<endl;    
+    }
+    
+    for (i=0; i<M; i++) {
+        for (j=0; j<N; j++) {
+            cout<<Array[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+    
+}
+
+
